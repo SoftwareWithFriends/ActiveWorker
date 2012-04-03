b450f7fbf7ff45ce3221fb6a437f5246e14b8f57
perf <perf@skarven.net>
2012-04-03 09:22:43 -0400

    [tim] explicitly loading up files in order

--- lib/active_worker.rb
+++ lib/active_worker.rb
@@ -1,10 +1,23 @@
 require 'mongoid'
 
-#Load sub directories first
-Dir.glob("#{File.dirname(__FILE__)}/active_worker/*/**/*.rb").each do |file|
-  require file
-end
+require 'active_worker/job_queue/run_remotely'
+require 'active_worker/job_queue/job_executer'
 
+require 'active_worker/behavior/acts_as_root_object'
+require 'active_worker/behavior/has_root_object'
+
+require 'active_worker/event'
+require 'active_worker/notification_event'
+require 'active_worker/finished_event'
+require 'active_worker/failure_event'
+
+require 'active_worker/configuration'
+require 'active_worker/controller'
+
+require 'active_worker/host_information'
+require 'active_worker/templatable'
+
+#Load anything else that is missing.
 Dir.glob("#{File.dirname(__FILE__)}/active_worker/**/*.rb").each do |file|
   require file
 end
