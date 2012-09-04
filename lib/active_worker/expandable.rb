b05cacfaa538841ae730c2d800d9be3ec579e4bc
perf <perf@skarven.net>
2012-09-04 15:46:17 -0400

    [ryan] added helper to expandable

--- lib/active_worker/expandable.rb
+++ lib/active_worker/expandable.rb
@@ -1,35 +1,39 @@
 module ActiveWorker
-    module Expandable
+  module Expandable
 
-      def self.included(base)
-        base.template_field :number_of_threads,  type: Integer, default: 1
-        base.template_field :number_of_workers,  type: Integer, default: 1
-      end
-
-      def expand_for_workers
-        expand_configuration(workers_to_expand)
-      end
-
-      def expand_for_threads
-        expand_configuration(threads_to_expand)
-      end
-
-      def expand_configuration(additional_configurations)
-        expanded_configs = [self]
-        additional_configurations.times do
-          expanded_configs << self.class.create(defined_fields.merge(parent_configuration: parent_configuration,
-                                                                     root_object_id: root_object_id))
-        end
-        expanded_configs
-      end
-
-      def workers_to_expand
-        (number_of_workers - 1)
-      end
-
-      def threads_to_expand
-        (number_of_threads - 1)
-      end
-
+    def self.included(base)
+      base.template_field :number_of_threads, type: Integer, default: 1
+      base.template_field :number_of_workers, type: Integer, default: 1
     end
+
+    def expand_for_workers
+      expand_configuration(workers_to_expand)
+    end
+
+    def expand_for_threads
+      expand_configuration(threads_to_expand)
+    end
+
+    def expand_configuration(additional_configurations)
+      expanded_configs = [self]
+      additional_configurations.times do
+        expanded_configs << created_expanded_configuration
+      end
+      expanded_configs
+    end
+
+    def created_expanded_configuration(options = {})
+      self.class.create(defined_fields.merge(parent_configuration: parent_configuration,
+                                             root_object_id: root_object_id).merge(options))
+    end
+
+    def workers_to_expand
+      (number_of_workers - 1)
+    end
+
+    def threads_to_expand
+      (number_of_threads - 1)
+    end
+
+  end
 end
